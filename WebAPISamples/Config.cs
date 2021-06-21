7dc485d004fa799ce6a74b9776b1e20af2976579
Marc <68268942+SybizMarc@users.noreply.github.com>
2021-06-21 17:00:35 +0930

    Add files via upload

---
+++ WebAPISamples/Config.cs
@@ -0,0 +1,8 @@
+﻿public static class CONFIG
+{
+    public static string ADDRESS = "https://www.google.com";
+    public static string DATABASE = "database1";
+    public static string USERNAME = "administrator";
+    public static string PASSWORD = "admin";
+    public static string KEYS = "key1";
+}
