b13338777ce3d32e68f62d86c45f9375ff4a941f
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2022-11-03 15:36:33 +1030

    Create DisableActiveFieldWhenJobHasWIP.vb

---
+++ BreakpointSamples/Implementing Custom Business Rules and Logic/DisableActiveFieldWhenJobHasWIP.vb
@@ -0,0 +1,22 @@
+Public Class DisableActiveFieldWhenJobHasWIP
+
+    'Scenario: Checks to see if the job has WIP, and if so, the Active checkbox is disabled
+    'Prerequisities: None
+    'Breakpoint: JobDisableTextEditor
+	
+  Public Sub Invoke(ByVal sender As System.Object, ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointPropertyDisabledEventArgs) 'Do not remove - SYBIZ
+
+    If e.PropertyName = "IsActive"
+      Dim job As Sybiz.Vision.Platform.JobCosting.Job = DirectCast(sender, Sybiz.Vision.Platform.JobCosting.Job)
+      If job.IsNew = false AndAlso job.IsActive Then                             
+        Dim jdi As Sybiz.Vision.Platform.JobCosting.JobDetailInfo = Sybiz.Vision.Platform.JobCosting.JobDetailInfo.GetObject(job.id)
+        Dim WIP As Decimal = jdi.OutstandingCosts
+        If WIP > 0 Then
+          e.Handled = True
+        End If
+      End If
+    End If
+	
+  End Sub
+
+End Class
