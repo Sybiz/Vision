a97d9975246ee990827c5b2d12190f14f4424558
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2022-11-04 14:57:23 +1030

    Create ForcePayandProcessForCashCustomer.vb

---
+++ BreakpointSamples/Modifying the Standard Workflow of Vision/ForcePayandProcessForCashCustomer.vb
@@ -0,0 +1,20 @@
+Public Class ForcePayandProcessForCashCustomer
+
+    'Scenario: Checks to see if the customer is a Cash customer and if so, forces the user to utilise Pay and Process, blocking attempts to just Process
+    'Prerequisities: None
+    'Breakpoint: BeforeSalesInvoiceProcess
+	
+	
+	Public Sub Invoke(ByVal transaction As Sybiz.Vision.Platform.Debtors.Transaction.SalesInvoice, ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointCancelEventArgs) 'Do not remove - SYBIZ
+
+		If transaction.CustomerDetails.TradingTerms.Term = TradingTermType.Cash Then
+			Dim salesform As Sybiz.Vision.WinUI.Base.SalesTransactionBase = DirectCast(e.Form, Sybiz.Vision.WinUI.Base.SalesTransactionBase)
+			If (salesform.HasReceipt = false)
+				e.Cancel = True
+				BreakpointHelpers.PerformRibbonButtonClick(e.Form, "Pay & Process")
+			End If
+		End If
+	
+	End Sub
+
+End Class
