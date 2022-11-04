be3fb88553f6f8e30b0534c993ab894b550824f8
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2022-11-04 15:45:05 +1030

    Create DisableCompanyandUserOutput.vb

---
+++ BreakpointSamples/Handling Emailing and Printing Transaction Documents/DisableCompanyandUserOutput.vb
@@ -0,0 +1,17 @@
+Public Class DisableCompanyandUserOutput
+
+    'Scenario: Checks to see if Company or User Output is enabled in a Sales Invoice, then disables both.
+    'Prerequisities: None
+    'Breakpoint: BeforeSalesInvoiceProcess
+	
+	
+	Public Sub Invoke(ByVal transaction As Sybiz.Vision.Platform.Debtors.Transaction.SalesInvoice, ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointCancelEventArgs) 'Do not remove - SYBIZ
+
+		If BreakpointHelpers.ReadRibbonEditorValue(e.Form, "Company Output") = True Or BreakpointHelpers.ReadRibbonEditorValue(e.Form, "User Output") = True Then
+			BreakpointHelpers.SetRibbonEditorValue(e.Form, "Company Output", False)
+			BreakpointHelpers.SetRibbonEditorValue(e.Form, "User Output", False)
+		End If
+	
+	End Sub
+
+End Class
