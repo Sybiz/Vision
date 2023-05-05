9445bca4fa23a14e95e5b33a1e17cdb5689f46e6
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2023-05-05 12:40:22 +0930

    Update AutoUpdateLastCostExamples.vb
    Adjusted considering comments.
    
    Question: is checking for location to location necessary in the context that the only way "elsewhere" is invoked IS via Location to Location? (Also noting that Elsewhere is 0 not -1, which is the Virtual Job location.)

--- BreakpointSamples/Automation of Data Entry or Data Selection Processes/AutoUpdateLastCostExamples.vb
+++ BreakpointSamples/Automation of Data Entry or Data Selection Processes/AutoUpdateLastCostExamples.vb
@@ -7,11 +7,15 @@ Public Class AutoUpdateLastCostExamples
   Public Sub Invoke(ByVal transaction As Sybiz.Vision.Platform.Inventory.Transaction.StockTransfer, ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointEventArgs) 'Do not remove - SYBIZ
 
     For each l As Sybiz.Vision.Platform.Inventory.Transaction.StockTransferLine In transaction.Lines
-      'If source location is elsewhere...
-      If l.SourceLocationDetails.Id = 0 Then
-      Sybiz.Vision.Platform.Inventory.Product.UpdateLocationLastCost(l.Product,l.Destination,l.UnitCost,true)
-      End If
-	  Next
+	'If source location is elsewhere...
+  	If  l.TransferType = Sybiz.Vision.Platform.Core.Enumerations.TransferType.LocationToLocation AndAlso l.Source = 0 Then		
+		Try 
+			Sybiz.Vision.Platform.Inventory.Product.UpdateLocationLastCost(l.Product,l.Destination,l.UnitCost,true)
+		Catch ex as Exception
+			BreakpointHelpers.ShowErrorMessage(e.Form, String.Format("Unable to update {0} last cost", l.ProductDetails.Code), ex.Message)
+		End Try		
+ 	End If
+    Next
 		
 	End Sub
 
@@ -19,10 +23,10 @@ Public Class AutoUpdateLastCostExamples
   Public Sub Invoke(ByVal transaction As Sybiz.Vision.Platform.Inventory.Transaction.ManufactureIssue, ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointEventArgs) 'Do not remove - SYBIZ
 
     For each l As Sybiz.Vision.Platform.Inventory.Transaction.ManufactureIssueLine In transaction.Lines
-      Sybiz.Vision.Platform.Inventory.Product.UpdateLastCost(l.Product,l.UnitCost,true)
+      Sybiz.Vision.Platform.Inventory.Product.UpdateLastCost(l.Product,l.UnitCost,false)
       
       'Alternatively, could use destination location here if one wanted to only update specific manufacturing location last cost
-      'Sybiz.Vision.Platform.Inventory.Product.UpdateLocationLastCost(l.ProductDetails.Id,l.DestinationLocation,l.UnitCost,true)
+      'Sybiz.Vision.Platform.Inventory.Product.UpdateLocationLastCost(l.ProductDetails.Id,l.DestinationLocation,l.UnitCost,false)
     Next
   
   End Sub
