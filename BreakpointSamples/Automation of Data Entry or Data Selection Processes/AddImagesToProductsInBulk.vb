664ea04806f5f26d1aaae9c853319d6f50263637
Marjon80 <62410826+Marjon80@users.noreply.github.com>
2023-05-11 21:54:58 +0930

    Update AddImagesToProductsInBulk.vb
    Replaced arrary with list
    Saved error messages to text file instead of multiple prompts

--- BreakpointSamples/Automation of Data Entry or Data Selection Processes/AddImagesToProductsInBulk.vb
+++ BreakpointSamples/Automation of Data Entry or Data Selection Processes/AddImagesToProductsInBulk.vb
@@ -6,61 +6,50 @@ Public Module ExternalApplicationCustomRibbonButtonClick
 
   Public Sub Invoke(ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointCustomRibbonButtonClickEventArgs)
     
-    If (e.Key = "PicUpload") Then
-			Dim errorlog As String() = New String(2) {}
-			Dim errorcount As Integer = 0
-					
-			Dim prod As Sybiz.Vision.Platform.Inventory.Product
-			Dim pic As Sybiz.Vision.Platform.Common.Picture
-			Dim dirname As String = BreakpointHelpers.GetStringValue(e.Form,"Directory Name","Please enter the directory name you wish to read images from","C:\")
-			Dim cont As DialogResult = BreakpointHelpers.ShowYesNoMessageBox(e.Form,"WARNING","Pressing yes will read all files in " + dirname +" and attach them to relevant products, do you want to continue?")
-			If cont = DialogResult.No Then
-				return
-			End If
-			System.Windows.Forms.Application.UseWaitCursor = true
-			Try
-				Dim dir As New DirectoryInfo(dirname)
-				For Each f As FileInfo In dir.GetFiles()
-					If f.Extension = ".jpg" Or f.Extension = ".png" Then
-						Try
-							'There is an assumption that the image names match codes within Vision
-							prod = Sybiz.Vision.Platform.Inventory.Product.GetObject(f.Name.Remove(f.Name.IndexOf(".")))								
-							If prod.Pictures.Count = 0 Then
-								pic = prod.Pictures.AddNew
-								pic.Image = File.ReadAllBytes(f.FullName)
-								If prod.IsSavable = True Then
-									prod.Save
-								Else
-									Throw New Exception("Product could not be saved!")
-								End If
-							Else
-								Throw New Exception("Pictures already exist against product!")
-							End If
-						Catch ex As Exception
-							errorlog(errorCount) = "Error processing file " & f.Name & ": " & ex.Message
-					        errorcount += 1
-					        If errorcount >= errorlog.Length Then
-					            Array.Resize(errorlog, errorlog.Length * 2)
-					        End If
-						End Try
-					End If
-				Next
-			Catch ex As Exception
-				BreakpointHelpers.ShowErrorMessage(e.Form,"ERROR",ex.Message)
-			Finally
-				System.Windows.Forms.Application.UseWaitCursor = false
-				errorlog = Array.FindAll(errorlog, Function(s) Not String.IsNullOrEmpty(s))
-				If errorlog.Length > 0 Then
-					Dim errcont As DialogResult = BreakpointHelpers.ShowYesNoMessageBox(e.Form,"ERRORS","During the operation, " + errorlog.Length + " errors were recorded. Press yes to see detail about each, or no to skip this.")
-					If errcont = DialogResult.Yes Then
-						For Each err As String In errorlog
-							BreakpointHelpers.ShowErrorMessage(e.Form,"ERROR",err)
-						Next
-					End If
-				End If
-				BreakpointHelpers.ShowInformationMessage(e.Form,"COMPLETE!","The operation has been completed")
-			End Try
-		End If
+	 If (e.Key = "PicUpload") Then
+		Dim errorlog As List(Of String) = New List(Of String)
 
+		Dim prod As Sybiz.Vision.Platform.Inventory.Product
+		Dim pic As Sybiz.Vision.Platform.Common.Picture
+		Dim dirname As String = BreakpointHelpers.GetStringValue(e.Form,"Directory Name","Please enter the directory name you wish to read images from","C:\")
+		Dim cont As DialogResult = BreakpointHelpers.ShowYesNoMessageBox(e.Form,"WARNING","Pressing yes will read all files in " + dirname +" and attach them to relevant products, do you want to continue?")
+		If cont = DialogResult.No Then
+			return
+		End If
+		System.Windows.Forms.Application.UseWaitCursor = true
+		Try
+			Dim dir As New DirectoryInfo(dirname)
+			For Each f As FileInfo In dir.GetFiles()
+				If f.Extension = ".jpg" Or f.Extension = ".png" Then
+					Try
+						'There is an assumption that the image names match codes within Vision
+						prod = Sybiz.Vision.Platform.Inventory.Product.GetObject(f.Name.Replace(f.Extension,""))							
+						If prod.Pictures.Count = 0 Then
+							pic = prod.Pictures.AddNew()
+							pic.Image = File.ReadAllBytes(f.FullName)
+							If prod.IsSavable = True Then
+								prod.Save
+							Else
+								Throw New Exception("Product could not be saved!")
+							End If
+						Else
+							Throw New Exception("Pictures already exist against product!")
+						End If
+					Catch ex As Exception
+						errorlog.Add(String.Format("Error processing file {0}:{1}",f.Name,ex.Message))
+					End Try
+				End If
+			Next
+		Catch ex As Exception
+			BreakpointHelpers.ShowErrorMessage(e.Form,"ERROR",ex.Message)
+		Finally
+			System.Windows.Forms.Application.UseWaitCursor = false
+			If errorlog.Count > 0 Then
+				BreakpointHelpers.ShowErrorMessage(e.Form,"ERRORS", String.Format("During the operation,{0} errors were recorded in VisionPictureImportErrors.txt", errorLog.Count))
+				System.IO.File.WriteAllLines(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"VisionPictureImportErrors.txt"),errorlog, System.Text.Encoding.ASCII)
+			End If
+			BreakpointHelpers.ShowInformationMessage(e.Form,"COMPLETE!","The operation has been completed")
+		End Try
+	End If
   End Sub
 End Module
