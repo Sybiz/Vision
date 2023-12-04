99207befd7d00903d96dd0a7769f38eea2a25453
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2023-12-04 15:59:16 +1030

    Create SelectHighestPriceScaleWhenNoneExists.vb (#12)

---
+++ BreakpointSamples/Implementing Custom Business Rules and Logic/SelectHighestPriceScaleWhenNoneExists.vb
@@ -0,0 +1,29 @@
+Public Class SelectHighestPriceScaleWhenNoneExists
+
+  'Scenario: When selecting a product, if the customer's price scale doesn't exist against a product, selects the price scale with the highest price when changing product or UoM (when price recalculations occur)
+  'Prerequisities: None, but there is an assumption in the below example that there is an intention to have at least one price scale per product.
+  'If that assumption is inaccurate, then this breakpoint will behave very poorly!
+  
+  'Note, this methodology will 'break' if Update Prices (or similar) is used. For extra $0 line protection, consider using a before save / process breakpoint as well, in the vein of https://github.com/Sybiz/Vision/blob/master/BreakpointSamples/Implementing%20Custom%20Business%20Rules%20and%20Logic/EnforceInvoiceQuantityBeforeTransactionSave.vb
+
+  'Breakpoint: SalesInvoiceCellValueChanged
+
+  Public Sub Invoke(ByVal transaction As Sybiz.Vision.Platform.Debtors.Transaction.SalesInvoice, ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointSalesDeliveryInvoiceCellValueChangedEventArgs(Of Sybiz.Vision.Platform.Debtors.Transaction.SalesInvoiceLine)) 'Do not remove - SYBIZ
+    If e.FieldName = "Account" OrElse e.FieldName = "UnitOfMeasure" Then
+      Dim p As Sybiz.Vision.Platform.Inventory.Transaction.IProductDetail = TryCast(e.Line, Sybiz.Vision.Platform.Inventory.Transaction.IProductDetail)
+      
+      If p IsNot Nothing AndAlso p.IsValid Then 
+        If p.ProductDetails.ProductPrices.Any(Function(x) x.Id = transaction.CustomerDetails.PriceScale) = False Then
+            Dim highPrice As Sybiz.Vision.Platform.Inventory.ProductPriceDetailInfo = p.ProductDetails.ProductPrices.OrderByDescending(Function(x) x.Price).FirstOrDefault()
+            If highPrice Is Nothing Then
+              e.Line.UnitChargeExclusive = 1000000
+              BreakpointHelpers.ShowErrorMessage(e.Form, "ERROR", "This product has no price scales, and this should be rectified immediately")
+            Else 
+                e.Line.UnitChargeExclusive = highprice.Price * e.Line.UnitOfMeasureFactor          
+            End If                    
+        End If
+      End If
+    End If
+  End Sub
+      
+End Class
