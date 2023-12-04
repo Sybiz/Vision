cdbabcab4dd7a7225015182b7f3d1394f7e93176
Marjon80 <62410826+Marjon80@users.noreply.github.com>
2023-12-04 15:04:15 +1030

    Create UseProductSalesTaxValueOnServiceInvoice.vb
    Update product lines with alternate sales tax code

---
+++ BreakpointSamples/Implementing Custom Business Rules and Logic/UseProductSalesTaxValueOnServiceInvoice.vb
@@ -0,0 +1,17 @@
+﻿Public Class UseProductSalesTaxValueOnServiceInvoice
+  
+  'Scenario: Use the sales tax code assigned to a product, overriding the default behavior on service invoice
+  'Prerequisities: None
+  'Breakpoint: ServiceInvoiceServiceRequestChanged
+
+  Public Sub Invoke(ByVal transaction As Sybiz.Vision.Platform.Service.Transaction.ServiceInvoice, ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointEventArgs)
+    For Each line As Sybiz.Vision.Platform.Service.Transaction.ServiceInvoiceLine in transaction.Lines
+      If line.AccountType = ServiceLineType.IC Then
+        Dim salesTaxCodeId As Integer = Sybiz.Vision.Platform.Inventory.ProductDetailInfo.GetObject(line.Account).SalesTaxCodeId
+        If salesTaxCodeId <> 0 Then
+          line.TaxCode = salesTaxCodeId
+        End If					
+      End If
+    Next
+  End Sub
+End Class
