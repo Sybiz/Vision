c1e661286a5c9e90c4db8b2bf8b98f4509ce619e
Marjon80 <62410826+Marjon80@users.noreply.github.com>
2023-12-20 15:37:46 +1030

    Update ChangeSkinonLogin.vb

--- BreakpointSamples/Modifying the Standard Workflow of Vision/ChangeSkinonLogin.vb
+++ BreakpointSamples/Modifying the Standard Workflow of Vision/ChangeSkinonLogin.vb
@@ -5,8 +5,8 @@
     'Breakpoint: VisionCompanyLaunched
 
     Public Sub Invoke(ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointEventArgs)
-      Dim UID As Integer = Sybiz.Vision.Platform.Security.Principal.CurrentPrincipal.UserId
-      If UID = 5 Then
+      Dim UserID As Integer = Sybiz.Vision.Platform.Security.Principal.CurrentPrincipal.UserId
+      If UserID = 5 Then
         'Must use "Bezier", as we only support the one style. The Pallette of the skin is what can be changed.
         DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle(DevExpress.LookAndFeel.SkinStyle.Bezier, "High Contrast Black") 
       End If
