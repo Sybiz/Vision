6021e01eef28de7830b8c76b491880925a9411a2
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2023-12-20 14:01:55 +1030

    Create ChangeSkinonLogin.vb

---
+++ BreakpointSamples/Modifying the Standard Workflow of Vision/ChangeSkinonLogin.vb
@@ -0,0 +1,15 @@
+﻿Public Class ChangeSkinonLogin
+
+    'Scenario: Automatically change the skin that a user is using; this example details the idea of a particular user wanting a different skin for the given company
+    'Prerequisities: None
+    'Breakpoint: VisionCompanyLaunched
+
+    Public Sub Invoke(ByVal e As Sybiz.Vision.Platform.Admin.Breakpoints.BreakpointEventArgs)
+      Dim UID As Integer = Sybiz.Vision.Platform.Security.Principal.CurrentPrincipal.UserId
+      If UID = 5 Then
+        'Must use "Bezier", as we only support the one style. The Pallette of the skin is what can be changed.
+        DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle(DevExpress.LookAndFeel.SkinStyle.Bezier, "Neon Lollipop") 
+      End If
+    End Sub
+
+End Class
