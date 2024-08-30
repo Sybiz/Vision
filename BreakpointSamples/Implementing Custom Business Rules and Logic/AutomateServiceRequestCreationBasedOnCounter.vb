95512d60dba8c6283b0d2da60bb128dc0372a7f6
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2024-08-30 12:10:02 +0930

    Update AutomateServiceRequestCreationBasedOnCounter.vb
    Parameter name cleanup

--- BreakpointSamples/Implementing Custom Business Rules and Logic/AutomateServiceRequestCreationBasedOnCounter.vb
+++ BreakpointSamples/Implementing Custom Business Rules and Logic/AutomateServiceRequestCreationBasedOnCounter.vb
@@ -8,27 +8,27 @@ Public Class AutomateServiceRequestCreationBasedOnCounter
       For each l As Sybiz.Vision.Platform.Service.Transaction.MeterReadingLine In transaction.Lines
         Dim param As New Dictionary(Of String,Object)
         param.Add("@Id",l.ServiceItem)
-        Dim lastread As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT LastReadingInterval FROM sv.ServiceItemExtendedProperty WHERE ObjectId = @Id",param)
-        Dim interval As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ReadingInterval FROM sv.ServiceItemExtendedProperty WHERE ObjectId = @Id",param)
-        Dim readbreak As Integer = lastread + interval
-        If l.Reading >= readbreak Then
+        Dim lastRead As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT LastReadingInterval FROM sv.ServiceItemExtendedProperty WHERE ObjectId = @Id",param)
+        Dim readingInterval As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ReadingInterval FROM sv.ServiceItemExtendedProperty WHERE ObjectId = @Id",param)
+        Dim readBreak As Integer = lastRead + readingInterval
+        If l.Reading >= readBreak Then
 
-          Dim sr As Sybiz.Vision.Platform.Service.ServiceRequest = Sybiz.Vision.Platform.Service.ServiceRequest.GetObject(0)
-          sr.Description = String.Format("Regular Counter Service For {0}",l.ServiceItemDetails.Description)
-          sr.ServiceItem = l.ServiceItem
+          Dim serviceRequest As Sybiz.Vision.Platform.Service.ServiceRequest = Sybiz.Vision.Platform.Service.ServiceRequest.GetObject(0)
+          serviceRequest.Description = String.Format("Regular Counter Service For {0}",l.ServiceItemDetails.Description)
+          serviceRequest.ServiceItem = l.ServiceItem
           'Assumption here is that Service Request Template is set up for a 'regular' service; this can be changed as needed / wanted.
-          Dim rt As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ServiceRequestTemplateId FROM sv.ServiceRequestTemplate WHERE Code = 'REG'",Nothing)
-          sr.RequestTemplate = rt
+          Dim requestTemplate As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ServiceRequestTemplateId FROM sv.ServiceRequestTemplate WHERE Code = 'REG'",Nothing)
+          serviceRequest.RequestTemplate = requestTemplate
           Try
-            If sr.IsSavable = true Then
-              sr = sr.Save
+            If serviceRequest.IsSavable = true Then
+              serviceRequest = serviceRequest.Save
               BreakpointHelpers.ShowInformationMessage(e.Form,"SR Created",String.Format("Service Request {0} created for {1} hour service for {2}",sr.ServiceRequestNumber,readbreak,l.ServiceItemDetails.Description))
               End If
           Catch ex As Exception
             BreakpointHelpers.ShowErrorMessage(e.Form,"ERROR",ex.Message)
           End Try
 
-          param.Add("@readbreak",readbreak)
+          param.Add("@readbreak",readBreak)
           BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"UPDATE sv.ServiceItemExtendedProperty SET LastReadingInterval = @readbreak WHERE ObjectId = @Id",param)
         End If
       Next
