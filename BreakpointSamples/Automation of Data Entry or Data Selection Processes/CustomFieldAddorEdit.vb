bfb4403e7dd8937ce184cd2b4ff0575000266263
sybethanlevy <117334437+sybethanlevy@users.noreply.github.com>
2024-12-03 17:21:42 +1030

    Update CustomFieldAddorEdit.vb
    Changed nullable syntax to be more in-line with common practice

--- BreakpointSamples/Automation of Data Entry or Data Selection Processes/CustomFieldAddorEdit.vb
+++ BreakpointSamples/Automation of Data Entry or Data Selection Processes/CustomFieldAddorEdit.vb
@@ -15,7 +15,7 @@
 			
 			'Variable could be passed in here to look for a specific custom field, or some modification to the query to check for it being a certain type, or similar.
 			'We are searching for any value; if nothing returned, it is assumed that the custom field does not exist.
-			Dim customFieldId? As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomField'",60,Nothing)
+			Dim customFieldId As Integer? = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomField'",60,Nothing)
 			
 			'Note that the below examples are EXHAUSTIVE; if you press the button four times, you'll get an error (since you'll try to save a custom field with the same name).
 			'Below code is an example only to demonstrate how to perform each "task".
@@ -33,7 +33,7 @@
 				newCustomField.IsActive = true
 			End If
 					        
-			Dim customFieldEnumId? As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomFieldEnum'",60,Nothing)
+			Dim customFieldEnumId As Integer? = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomFieldEnum'",60,Nothing)
 			
 			'Example revolving around enumerated custom field
 			If customFieldEnumId.HasValue = True Then
@@ -53,7 +53,7 @@
 				newEnumValue.Value = "New"
 			End If
 					
-			Dim customFieldStaticId? As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomFieldStatic'",60,Nothing)
+			Dim customFieldStaticId As Integer? = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomFieldStatic'",60,Nothing)
 			
 			'Example revolving around "Static List" custom field
 			If customFieldStaticId.HasValue = True Then
