d0b2fdf478d8c7b814024602f368a6adee7c707e
Marjon80 <62410826+Marjon80@users.noreply.github.com>
2025-01-20 09:43:15 +1030

    Add transaction custom field example

--- BreakpointSamples/Automation of Data Entry or Data Selection Processes/CustomFieldAddorEdit.vb
+++ BreakpointSamples/Automation of Data Entry or Data Selection Processes/CustomFieldAddorEdit.vb
@@ -19,7 +19,7 @@
 			
 			'Note that the below examples are EXHAUSTIVE; if you press the button four times, you'll get an error (since you'll try to save a custom field with the same name).
 			'Below code is an example only to demonstrate how to perform each "task".
-			        
+				
 			If customFieldId.HasValue = True Then
 				Dim existingCustomField As Sybiz.Vision.Platform.Admin.ExtendedPropertyTemplateItem = productCustomFieldList.TemplateItems.FirstOrDefault(Function(x) x.Id = customFieldId.Value)
 				existingCustomField.Name = existingCustomField.Name + "_suffix"
@@ -32,7 +32,7 @@
 				newCustomField.SortOrder = 0
 				newCustomField.IsActive = true
 			End If
-					        
+						
 			Dim customFieldEnumId As Integer? = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomFieldEnum'",60,Nothing)
 			
 			'Example revolving around enumerated custom field
@@ -80,7 +80,49 @@
 		Catch ex As Exception
 			BreakpointHelpers.ShowErrorMessage(e.Form, "DANGER WILL ROBINSON!", ex.Message)
 		End Try
-    End If
+	End If
+					
+	If e.Key = "TransactionCustomFieldExample" Then
+		Try
+			      
+			'This example looks at the purchase header custom field list; looking at admin.ExtendedPropertyTemplate will let you see what options you hav
+			'The same principles apply when updating line based custom fields such as Purchase Line
+			Dim templateId As Integer = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text, "SELECT ExtendedPropertyTemplateId FROM admin.ExtendedPropertyTemplateExpanded WHERE ObjectType = 'Purchase Header'", 60,Nothing)
+			Dim purchaseTransactionsCustomFieldList As Sybiz.Vision.Platform.Admin.ExtendedPropertyGroupTemplate = Sybiz.Vision.Platform.Admin.ExtendedPropertyGroupTemplate.GetObject(templateId)
+			
+			'Variable could be passed in here to look for a specific custom field, or some modification to the query to check for it being a certain type, or similar.
+			'We are searching for any value; if nothing returned, it is assumed that the custom field does not exist.
+			Dim customFieldId As Integer? = BreakpointHelpers.ExecuteScalarCommand(CommandType.Text,"SELECT ExtendedPropertyTemplateItemId FROM admin.ExtendedPropertyTemplateItem WHERE Name = 'BreakpointCustomField'",60,Nothing)
+			
+			'Below code is an example only to demonstrate to create a basic custom field against a transaction header/line							        
+			If customFieldId.HasValue = False Then
+				Dim newCustomField As Sybiz.Vision.Platform.Admin.ExtendedPropertyTemplateGroupItem = Sybiz.Vision.Platform.Admin.ExtendedPropertyTemplateGroupItem.NewObject(purchaseTransactionsCustomFieldList.Id, purchaseTransactionsCustomFieldList.Tabs)
+				Dim transactionType As Sybiz.Vision.Platform.Admin.ExtendedPropertyGroupTransactionType
+				purchaseTransactionsCustomFieldList.TemplateGroupItems.Add(newCustomField)
+				
+				newCustomField.Name = "BreakpointCustomField"
+				newCustomField.Type = Sybiz.Vision.Platform.Core.ExtendedProperties.ExtendedPropertyType.String
+				newCustomField.FieldLength = 255
+				newCustomField.TabDescription = "Custom Tab 1"
+				newCustomField.SortOrder = 0
+				
+				'Need to mark specific transaction types as inactive if not used on all transaction types, defaults to all transactions
+				'This is instead of marking the custom field as active, to make it inactive all transaction types must be marked as false
+				For Each transactionType In newCustomField.TransactionTypes
+					If transactionType.Text <> "PurchaseOrder" Then
+						transactionType.IsActive = False
+					End If
+				Next
+			
+			End If
+													
+			'Specifically need to save the "header", not the child object.
+			purchaseTransactionsCustomFieldList = purchaseTransactionsCustomFieldList.Save()
+		Catch ex As Exception
+			BreakpointHelpers.ShowErrorMessage(e.Form, "DANGER WILL ROBINSON!", ex.Message)
+		End Try
+	End If
+					
   
   End Sub
 End Class
